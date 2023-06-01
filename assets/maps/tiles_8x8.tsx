2a576c864612e827e4c8992a27b0d8242de27905
Senén Martínez Acame <135156602+SenenAcame@users.noreply.github.com>
2023-06-01 10:41:27 +0200

    Assets, cfg, exp y archivos

---
+++ assets/maps/tiles_8x8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tiles_suelo8x8" tilewidth="8" tileheight="8" tilecount="72" columns="8">
+ <image source="Tiles_suelo8x8.png" width="64" height="72"/>
+</tileset>
